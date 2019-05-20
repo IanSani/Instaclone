afeafa33bc3d0cdec46273c64fabedce9966c35d
IanSani <iansani259@gmail.com>
2019-05-20 11:31:36 +0300

    created class upload form  in forms.py

--- images/forms.py
+++ images/forms.py
@@ -6,3 +6,8 @@ class NewProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude =['posted_by','profile','likes']

8a8a2299ba0dfa7e959f1b6f395b65634e38676e
IanSani <iansani259@gmail.com>
2019-05-20 17:35:51 +0300

    Adde image test class to check display of added images

--- images/tests.py
+++ images/tests.py
@@ -23,3 +23,22 @@ class ProfileTest(TestCase):
         self.profile.save()
         profile = Profile.find_profile('zyzu')
         self.assertTrue(len(profile) > 0)
+
+class ImageTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='zyzu')
+        self.profile = Profile.objects.create(user = self.user,bio = 'blow away')
+
+        self.image = Image.objects.create(posted_by = self.user,
+                                          profile = self.profile,
+                                          caption ='turn up',
+                                          likes = 0)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+    def test_get_images(self):
+        self.image.save()
+        image = Image.get_images()
+        self.assertTrue(len(image) == 1)
